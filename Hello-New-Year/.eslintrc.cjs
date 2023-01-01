9c2c580f2cf69830c91ef0f10b9ac22557a20e99
Michael Obst <michael.obst@telekom.de>
2023-01-01 19:26:58 +0100

    add scale

--- Hello-New-Year/.eslintrc.cjs
+++ Hello-New-Year/.eslintrc.cjs
@@ -18,6 +18,9 @@ module.exports = {
       ]
     }
   ],
+  rules: {
+    'vue/no-deprecated-slot-attribute': 'off'
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
--- Hello-New-Year/src/main.ts
+++ Hello-New-Year/src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { defineCustomElements } from '@telekom/scale-components/loader'
+import '@telekom/scale-components/dist/scale-components/scale-components.css'
 
 import './assets/main.css'
+
+defineCustomElements()
 
 const app = createApp(App)
 
