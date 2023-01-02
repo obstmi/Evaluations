4235ea96956482afe784ab7533b58b9223c35d0d
Michael Obst <michael.obst@telekom.de>
2023-01-02 16:44:03 +0100

    add footer

--- Hello-New-Year/src/App.vue
+++ Hello-New-Year/src/App.vue
@@ -1,13 +1,8 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
+    <div>
       <HelloWorld msg="You did it!" />
 
       <nav>
@@ -18,68 +13,37 @@ import HelloWorld from './components/HelloWorld.vue'
   </header>
 
   <RouterView />
+  <scale-app-footer claim-lang="de" :logoHref="telekomHomeUrl" :footerNavigation="footerNavigation"></scale-app-footer>
 </template>
 
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+
+const telekomHomeUrl = 'https://www.telekom.de/start'
+const footerNavigation = [
+  {
+    name: 'Kontaktformular',
+    id: 'Contact',
+    href: 'https://www.telekom.de/hilfe'
+  },
+  {
+    name: 'Nutzungsbedingungen',
+    id: 'Terms and conditions',
+    href: 'https://www.telekom.de/is-bin/INTERSHOP.enfinity/WFS/EKI-PK-Site/-/-/-/ViewAGB-TextQuery?SearchText=&wt_mc=alias_1156_agb'
+  },
+  {
+    name: 'Impressum',
+    id: 'Impressum',
+    href: 'https://www.telekom.de/start/impressum'
+  },
+  { name: 'Datensschutz',
+    id: 'Data privacy',
+    href: 'https://www.telekom.de/datenschutz-ganz-einfach',
+    icon: 'alert-imprint-dataprivacy'
+  }
+]
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- Hello-New-Year/src/main.ts
+++ Hello-New-Year/src/main.ts
@@ -4,8 +4,6 @@ import router from './router'
 import { defineCustomElements } from '@telekom/scale-components/loader'
 import '@telekom/scale-components/dist/scale-components/scale-components.css'
 
-import './assets/main.css'
-
 defineCustomElements()
 
 const app = createApp(App)
