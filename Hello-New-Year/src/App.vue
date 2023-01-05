81f8e7443d6c7c464f1a6c9ece72c22c50ba2aa9
Michael Obst <michael.obst@telekom.de>
2023-01-05 13:54:22 +0100

    add header

--- Hello-New-Year/src/App.vue
+++ Hello-New-Year/src/App.vue
@@ -1,24 +1,34 @@
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div>
+    <scale-app-shell>
+      <scale-app-header slot="header" id="header">
+        <div slot="menu-main">
+          <scale-nav-main name="Custom Item #1"></scale-nav-main>
+          <scale-nav-main name="Custom Item #2"></scale-nav-main>
+          <scale-nav-main name="Custom Item #3"></scale-nav-main>
+          <scale-nav-main name="Custom Item #4"></scale-nav-main>
+        </div>
+      </scale-app-header>
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+      <div>
       <HelloWorld msg="You did it!" />
 
+      <!--
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterView />
       </nav>
+      -->
     </div>
-  </header>
+      <!-- https://vuejs.org/guide/extras/web-components.html#using-custom-elements-in-vue -->
+      <scale-app-footer slot="footer" id="footer" claim-lang="de" :logoHref.prop="telekomHomeUrl" :footerNavigation.prop="footerNavigation"></scale-app-footer>
 
-  <RouterView />
-  <!-- https://vuejs.org/guide/extras/web-components.html#using-custom-elements-in-vue -->
-  <scale-app-footer claim-lang="de" :logoHref.prop="telekomHomeUrl" :footerNavigation.prop="footerNavigation"></scale-app-footer>
+    </scale-app-shell>
+
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+// import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 
 const telekomHomeUrl = 'https://www.telekom.de/start'
